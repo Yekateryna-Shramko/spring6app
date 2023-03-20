4b5bdc50ec687b51e532d72cbae0e674cfaf0e24
Yekateryna Shramko <kirapteryx@gmail.com>
2023-03-20 18:24:53 +0100

    Initial commit

---
+++ src/main/java/io/github/ysharmko/spring6app/Spring6appApplication.java
@@ -0,0 +1,13 @@
+package io.github.ysharmko.spring6app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6appApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6appApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ysharmko/spring6app/Spring6appApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ysharmko.spring6app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6appApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
