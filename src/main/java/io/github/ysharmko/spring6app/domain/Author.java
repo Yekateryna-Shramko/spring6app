f4937bd49b35c3472f0c28daa3675910484d4220
Yekateryna Shramko <kirapteryx@gmail.com>
2023-03-20 20:45:14 +0100

    Add jpa entities

---
+++ src/main/java/io/github/ysharmko/spring6app/domain/Author.java
@@ -0,0 +1,49 @@
+package io.github.ysharmko.spring6app.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> book;
+
+    public Set<Book> getBook() {
+        return book;
+    }
+
+    public void setBook(Set<Book> book) {
+        this.book = book;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
